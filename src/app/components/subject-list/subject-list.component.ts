10bbcada1b051294d339a91a553077928b3c8a3e
Thomas <thomaspapas470@gmail.com>
2024-09-06 11:17:18 +0200

    Added subject-list.html

--- src/app/components/subject-list/subject-list.component.ts
+++ src/app/components/subject-list/subject-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subject } from '../../interfaces';
 import { SubjectListService } from '../../services/subject-list/subject-list.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-subject-list',
   standalone: true,
-  imports: [],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './subject-list.component.html',
   styleUrl: './subject-list.component.scss'
 })
@@ -14,13 +15,8 @@ export class SubjectListComponent {
   subjects$ : Observable<Subject[]>;
   constructor(
     private readonly _service : SubjectListService
-  ){
-    
+  ){  
   this.subjects$ = this._service.getSubjects();
-
-
-
-
   }
 
 }
