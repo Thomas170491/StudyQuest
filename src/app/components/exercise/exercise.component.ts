3e54a69f0b80c29127f151b84e2ede69f0fed9e4
Thomas <thomaspapas470@gmail.com>
2024-09-09 16:11:55 +0200

    Added Outèut event emitter on subject-list and created $question variable in exercise

--- src/app/components/exercise/exercise.component.ts
+++ src/app/components/exercise/exercise.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ExerciseService } from '../../services/exercises/exercises.service';
 import { SubjectListService } from '../../services/subject-list/subject-list.service';
 import { ExerciseData } from '../../data';
+import { Observable } from 'rxjs';
+import { Exercise } from '../../interfaces';
 
 @Component({
   selector: 'app-exercise',
@@ -11,12 +13,15 @@ import { ExerciseData } from '../../data';
   styleUrl: './exercise.component.scss'
 })
 export class ExerciseComponent {
+  subjectId : string ='';
+  question$ : Observable<Exercise[]>;
   constructor(
     private readonly _exerciseservice : ExerciseService,
     private readonly _subjectservice : SubjectListService
 
   ){
-    
-
+    this.question$= this._exerciseservice.getExerciseBySubject(this.subjectId)
   }
+  
+ 
 }
