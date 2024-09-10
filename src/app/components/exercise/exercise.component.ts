f6eef876453fb1a900626c506efc7bd2afe251c1
Thomas <thomaspapas470@gmail.com>
2024-09-10 11:49:53 +0200

    Latest version of exercise component

--- src/app/components/exercise/exercise.component.ts
+++ src/app/components/exercise/exercise.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { ExerciseService } from '../../services/exercises/exercises.service';
-import { SubjectListService } from '../../services/subject-list/subject-list.service';
 import { Observable } from 'rxjs';
 import { Exercise } from '../../interfaces';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
-import { SubjectListComponent } from "../subject-list/subject-list.component";
-import { Router } from '@angular/router';
+import { SubjectListComponent } from "../subject-list/subject-list.component";;
 import { FilterbySubjectIdPipe } from '../../pipes/filterby-subject-id.pipe';
 @Component({
   selector: 'app-exercise',
@@ -19,8 +17,7 @@ export class ExerciseComponent {
   questions$ : Observable<Exercise[]>;
   constructor(
     private readonly _exerciseservice : ExerciseService,
-    private readonly _subjectservice : SubjectListService,
-    private router: Router,
+    
 
   ){
     this.questions$= this._exerciseservice.getExercises()
