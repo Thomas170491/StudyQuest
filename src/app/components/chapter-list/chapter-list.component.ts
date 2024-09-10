04ac145bc4ff3700ca70da0fa77e3450e37a1f5e
Thomas <thomaspapas470@gmail.com>
2024-09-11 01:38:46 +0200

    Added Input chapterId

--- src/app/components/chapter-list/chapter-list.component.ts
+++ src/app/components/chapter-list/chapter-list.component.ts
@@ -3,34 +3,31 @@ import { Chapter } from '../../interfaces';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { ChapterService } from '../../services/chapters.service';
 import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chapter-list',
   standalone: true,
   imports: [NgFor, NgIf, AsyncPipe],
   templateUrl: './chapter-list.component.html',
-  styleUrls: ['./chapter-list.component.scss',] // Fixed typo here
+  styleUrls: ['./chapter-list.component.scss',] 
 })
 export class ChapterListComponent {
   chapters$: Observable<Chapter[]>;
 
   constructor(
-    private router: Router,
+
     private readonly _service: ChapterService,
   ) {
     this.chapters$ = this._service.getChapters();
   }
 
-  @Output() selectedEvent: EventEmitter<string> = new EventEmitter<string>(); 
+  @Output() selectedChapter: EventEmitter<string> = new EventEmitter<string>(); 
 
-  redirectToSubjectList(): void { 
-    this.router.navigate(['/subject-list']); 
-  }
   onChapterSelect(id: string): void {
-    this.selectedEvent.emit(id);
-    this.redirectToSubjectList();
-  }
+    this.selectedChapter.emit(id);
+   
 }
+
+  }
 
 
