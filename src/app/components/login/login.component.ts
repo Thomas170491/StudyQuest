b58a634a09bf56099684ec281f2ddea2c660d6c0
Thomas <thomaspapas470@gmail.com>
2024-09-21 14:11:31 +0200

    finished login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { getAuth,Auth, GoogleAuthProvider,signInWithPopup } from '@angular/fire/auth';
-import { Router } from '@angular/router'; 
-import { FirestoreService } from '../../services/firestore/firestore.service';
-import { UserService } from '../../services/users/user-service.service';
-import { map } from 'rxjs';
-import { doc, getDoc } from 'firebase/firestore';
-import { Firestore } from '@angular/fire/firestore';
-import { Progress, User } from '../../interfaces';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,67 +11,16 @@ import { Progress, User } from '../../interfaces';
 export class LoginComponent {
   
   constructor(
-    private router : Router,
-    private readonly _auth : Auth,
-    private readonly _firestoreService : FirestoreService,
-    private readonly _firestore : Firestore,
-    private readonly _userService : UserService
-   ){}
-    
-    authentificateUser = async () => {
-      const provider = new GoogleAuthProvider();
-      const credential = await signInWithPopup(this._auth, provider);
-      const user = credential.user;
-      console.log(user);
-      
-      if(user){
-        const userDocRef = doc(this._firestore,'Users', user.uid);
-        const userDoc = await getDoc(userDocRef);
+   
+    private readonly _authService: AuthService
+  ) {}
 
-        let completedExercises: Progress[] = [];
+  authentificate = async () => {
+    await this._authService.authentificateUser();
+  }
 
-        const userExist : User = {
-          id: user.uid,
-          username: user.displayName ?? '',
-          email: user.email ?? '',
-          profilePictureUrl: user.photoURL,
-          schoolYear: '9ème',
-          group: 'R3',
-          isAuth: true,
-          completedExercises: []
-        };
-
-        if (!userDoc.exists()) {
-          this._userService.addUser(userExist);
-          this._firestoreService.addData('Users', userExist);
-        } else {
-          const existingUserData = userDoc.data() as User; 
-          completedExercises = existingUserData.completedExercises;
-          this._userService.updateUser({ ...existingUserData, isAuth: true, completedExercises : completedExercises });
-          this._firestoreService.updateData('Users', user.uid, { isAuth : true, completedExercises : completedExercises });
-        }
-        this.router.navigate(['/select']);
-      }
-      this.router.navigate(['/home']);
-      }
-
-      signOut = async () => {
-        const userId = this._auth.currentUser?.uid;
-        await this._auth.signOut();
-        
-        if (userId) {
-          this._firestoreService.updateData('Users', userId, { isAuth: false });
-          this._userService.getUserById(userId).pipe(
-            map(user => {
-            if (user) {
-              this._userService.updateUser({ ...user, isAuth: false });
-            }
-          }));
-        }
-        this.router.navigate(['/home']);
-      }
-      
-   }
-  
-
-
+  signOut = async () => {
+    await this._authService.signOut();
+   
+  }
+}
